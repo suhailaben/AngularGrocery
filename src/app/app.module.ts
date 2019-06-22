06e0a7ecf808319c7d2d5a803896e3a1774f5299
suhailaben <40865556+suhailaben@users.noreply.github.com>
2019-06-22 15:59:38 -0700

    main complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GroceryComponent } from './grocery/grocery.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { GroceryComponent } from './grocery/grocery.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, GroceryComponent]
 })
 export class AppModule { }
--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -3,10 +3,53 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grocery',
   templateUrl: './grocery.component.html',
-  styleUrls: ['../assets/css/bootstrap.min.css']
+  styleUrls: ['./grocery.component.css', '../../assets/css/bootstrap.min.css']
 })
 export class GroceryComponent implements OnInit {
 
+  task = {
+    name: '',
+    id: 0
+  };
+  tasks = [];
+
+  onClick(){
+    if(this.task.id == 0){
+      this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false});
+    }
+    
+    this.task = {
+      name: '',
+      id: 0
+    };
+  }
+  
+  onEdit(item){
+    this.task = item;
+  }
+
+  onDelete(item){
+    for(var i = 0;i < this.tasks.length; i++){
+      if(item.id == this.tasks[i].id){
+        this.tasks.splice(i,1);
+        break;
+      }
+    }
+  }
+
+  onStrike(item){
+    for(var i = 0;i < this.tasks.length; i++){
+      if(item.id == this.tasks[i].id){
+        if(this.tasks[i].strike){
+          this.tasks[i].strike = false;
+        }
+        else{
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit() {
